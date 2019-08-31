4f07023692ec918f1bf7e52654c942854a5faa9a
minz1 <emerytang@gmail.com>
2019-08-30 21:16:21 -0500

    sm6150-common: bluetooth: undef BTM_DEF_LOCAL_NAME
    * We are going to migrate to prop in device trees instead
    
    Co-authored-by: Isaac Chen <isaacchen@isaacchen.cn>
    Change-Id: Idd31aa741dae5e533e6a7642abbb28ac4ce643b6
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -25,48 +25,6 @@
 
 #pragma push_macro("PROPERTY_VALUE_MAX")
 
-#include <cutils/properties.h>
-#include <string.h>
-
-#include "osi/include/osi.h"
-
-typedef struct {
-    const char *product_device;
-    const char *product_region;
-    const char *product_model;
-} device_t;
-
-static const device_t devices[] = {
-    {"davinci", "CN", "Xiaomi Redmi K20"},
-    {"davinci", "GLOBAL", "Xiaomi Mi 9T"},
-    {"davinciin", "INDIA", "Xiaomi Redmi K20"},
-    {"phoenix", "ALL", "Redmi K30"},
-    {"phoenixin", "INDIA", "POCO X2"},
-    {"violet", "ALL", "Xiaomi Redmi Note 7 Pro"},
-};
-
-static inline const char *BtmGetDefaultName()
-{
-    char product_device[PROPERTY_VALUE_MAX];
-    char product_region[PROPERTY_VALUE_MAX];
-    property_get("ro.product.device", product_device, "");
-    property_get("ro.boot.hwc", product_region, "");
-
-    for (unsigned int i = 0; i < ARRAY_SIZE(devices); i++) {
-        device_t device = devices[i];
-
-        if (strcmp(device.product_device, product_device) == 0 &&
-               (strcmp(device.product_region, product_region) == 0 ||
-               strcmp(device.product_region, "ALL") == 0)) {
-           return device.product_model;
-       }
-    }
-
-    // Fallback to ro.product.model
-    return "";
-}
-
-#define BTM_DEF_LOCAL_NAME BtmGetDefaultName()
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
