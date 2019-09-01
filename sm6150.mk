51487d3b0887a9f5506c4067e0599e629bd10e58
NurKeinNeid <mralexman3000@gmail.com>
2019-09-01 22:35:23 +0200

    sm6150-common: Remove LiveDisplay and Trust HAL
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -223,10 +223,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.xiaomi_sm6150
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
@@ -353,10 +349,6 @@ PRODUCT_PACKAGES += \
 # Touchscreen
 PRODUCT_PACKAGES += \
     libtinyxml2
-
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
