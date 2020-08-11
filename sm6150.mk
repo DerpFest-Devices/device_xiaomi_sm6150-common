d3bd4816e38aceca73414041fa8f1678a97a2e7e
kazerine <kamikaze.mrinal@gmail.com>
2020-08-12 00:18:36 +0530

    sm6150-common: Get rid of megvii dependencies in camera.qcom.so
    Change-Id: I97058c320d06819a468ebe38e2704360e9aa91db
    Signed-off-by: Arian <arian.kulmer@web.de>
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -113,6 +113,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     libdng_sdk.vendor \
     libgui_vendor \
+    libshim_megvii \
     libpiex_shim \
     libxml2 \
     vendor.qti.hardware.camera.device@1.0.vendor
