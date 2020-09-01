1152e44db92fcb1b0ad0cbb5ecfe2e8bcb8ce0af
pramod kotreshappa <kpramod@codeaurora.org>
2020-09-01 12:50:22 -0700

    sm6150-common: Update IBluetoothAudioProvidersFactory HAL to v2.1
    Change-Id: I396d2a28b8146a7c2018c15fa77452f7011fbac8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -111,7 +111,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl:32 \
     audio.bluetooth.default \
     libbthost_if \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
