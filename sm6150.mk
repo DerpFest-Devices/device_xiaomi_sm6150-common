498a9a9730ffeb3957e18ebf5d451e0f199bf107
adi8900 <adrianszymanski242@gmail.com>
2021-01-07 12:46:15 +0100

    Revert "sm6150-common: Exclude Snap overlays from RRO"
    This reverts commit c7e845bb8f87400c0b06d5fc8ac43f7b28fe9f26.
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -19,8 +19,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay/packages/apps/Snap
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
