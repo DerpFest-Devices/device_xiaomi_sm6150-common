9c30a638783b7d80e79bc2ce754753f50a45e34a
adi8900 <adrianszymanski242@gmail.com>
2021-06-03 20:00:11 +0200

    sm6150-common: Add camera.qcom.so patch into common tree [2/2]
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -70,6 +70,11 @@ function blob_fixup() {
         sed -i "s|0x10080|0|g" "${2}"
         sed -i "s|0x1F|0x0|g" "${2}"
     ;;
+    vendor/lib64/hw/camera.qcom.so)
+        "${PATCHELF}" --remove-needed "libMegviiFacepp-0.5.2.so" "${2}"
+        "${PATCHELF}" --remove-needed "libmegface.so" "${2}"
+        "${PATCHELF}" --add-needed "libshim_megvii.so" "${2}"
+    ;;
     vendor/etc/init/vendor.sensors.qti.rc )
     echo "    disabled" >> "${2}"
     ;;
