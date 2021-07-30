f08c9b6560c7ba640b5dd166284e2a93837f1a12
Nolen Johnson <johnsonnolen@gmail.com>
2021-07-30 15:33:23 +0200

    sm6150-common: Cleanup more dead targets
    FAILED:
    build/make/core/main.mk:1147: warning:  device/xiaomi/davinci/derp_davinci.mk includes non-existent modules in PRODUCT_PACKAGES
    Offending entries:
    Snap
    libtinycompress.vendor
    build/make/core/main.mk:1147: error: Build failed.
    15:23:36 ckati failed with: exit status 1
    
    Test ur device with:
    $(call enforce-product-packages-exist,)
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -88,7 +88,6 @@ PRODUCT_PACKAGES += \
     libsndmonitor \
     libspkrprot \
     libtinycompress \
-    libtinycompress.vendor \
     libvolumelistener
 
 PRODUCT_COPY_FILES += \
@@ -126,7 +125,6 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     libxml2 \
-    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Config Store
