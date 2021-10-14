11838d18c20168524b8aab7be7a5e4d7739a395d
adi8900 <adrianszymanski242@gmail.com>
2021-10-14 21:20:32 +0200

    sm6150-common: Fp target will be decommonized now.
    * fod devices will use -udfps variant and non fod without -udfps
    * move target into specific device trees, keep fp hal commonized with changes to support fod and no fod devices
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -176,9 +176,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm6150
-
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
