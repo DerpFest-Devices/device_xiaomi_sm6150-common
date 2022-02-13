e50bf3aa142e950b240549cbe9a5874753b411f9
adi8900 <adrianszymanski242@gmail.com>
2022-02-13 23:58:48 +0100

    sm6150-common: Ship missing qti_whitelist_system_ext.xml
    * from LA.QSSI.12.0.r1-05600-qssi.0
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -303,7 +303,8 @@ PRODUCT_COPY_FILES += \
 
 # QTI
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist_system_ext.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/qti_whitelist_system_ext.xml
 
 # Ramdisk
 PRODUCT_PACKAGES += \
