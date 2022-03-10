cf4eac65570a43c797a00abb68779f4757426499
Nilesh Gharde <quic_ngharde@quicinc.com>
2022-03-11 03:46:00 +0530

    Enable protected dynamic interface control
    Changes to Enable protected dynamic interface control.
    
    Added support to
    1. Get the engine lock state.
    2. Handle engine lock state event.
    3. Store all the sessions when the engine is locked.
    4. Restore all the sessions when the engine unlock event is received.
    
    Change-Id: I70e591a6dca2231fb762ef199d0d34f852198116
    CRs-fixed: 3130222

--- core/LocApiBase.h
+++ core/LocApiBase.h
@@ -137,6 +137,7 @@ protected:
     bool isInSession();
     const LOC_API_ADAPTER_EVENT_MASK_T mExcludedMask;
     bool isMaster();
+    EngineLockState mEngineLockState;
 
 public:
     inline void sendMsg(const LocMsg* msg) const {
@@ -203,6 +204,7 @@ public:
     void sendNfwNotification(GnssNfwNotification& notification);
     void reportGnssConfig(uint32_t sessionId, const GnssConfig& gnssConfig);
     void reportLatencyInfo(GnssLatencyInfo& gnssLatencyInfo);
+    void reportEngineLockStatus(EngineLockState engineLockState);
     void reportQwesCapabilities
     (
         const std::unordered_map<LocationQwesFeatureType, bool> &featureMap
@@ -345,6 +347,14 @@ public:
                                               LocApiResponse* adapterResponse=nullptr);
     virtual void getConstellationMultiBandConfig(uint32_t sessionId,
                                         LocApiResponse* adapterResponse=nullptr);
+
+    inline EngineLockState getEngineLockState() {
+        return mEngineLockState;
+    }
+
+    inline void setEngineLockState(EngineLockState engineLockState) {
+        mEngineLockState = engineLockState;
+    }
 };
 
 class ElapsedRealtimeEstimator {
