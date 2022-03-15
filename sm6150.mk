415c83c7119cf2421fc0abea993f73e9669d491e
LuK1337 <priv.luk@gmail.com>
2022-03-15 23:57:56 +0100

    sm6150-common: Switch to QTI USB HAL
    Change-Id: I51904112ca01adf4c49290d259940541e1664d45

--- sm6150.mk
+++ sm6150.mk
@@ -371,7 +371,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.3-service-qti
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
