dfd81f7fb457028e6afa469bc230fc85d55bac3d
Diniboy1123 <Diniboy1123@users.noreply.github.com>
2022-04-25 23:41:17 +0200

    sm6150-common: livedisplay: rename exposure adjustment node
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- livedisplay/AntiFlicker.cpp
+++ livedisplay/AntiFlicker.cpp
@@ -29,7 +29,7 @@ namespace V2_1 {
 namespace implementation {
 
 static constexpr const char* kAntiFlickerStatusPath =
-        "/sys/devices/platform/soc/soc:qcom,dsi-display/msm_fb_ea_enable";
+        "/sys/devices/platform/soc/soc:qcom,dsi-display/anti_flicker";
 
 Return<bool> AntiFlicker::isEnabled() {
     std::string buf;
