xiaomi-sm6150/android_device_xiaomi_sm6150-common

sm6150-common: livedisplay: Add anti flicker support
Change-Id: Ieb91a3efe0fc124e120f95f5ec197aa3c5cecb14

livedisplay/AntiFlicker.cpp
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "AntiFlickerService"
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android-base/strings.h>
+
+#include "AntiFlicker.h"
+
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace V2_0 {
+namespace implementation {
+
+static constexpr const char* kDcDimmingStatusPath =
+        "/sys/devices/platform/soc/soc:qcom,dsi-display/msm_fb_ea_enable";
+
+Return<bool> AntiFlicker::isEnabled() {
+    std::string buf;
+    if (!android::base::ReadFileToString(kDcDimmingStatusPath, &buf)) {
+        LOG(ERROR) << "Failed to read " << kDcDimmingStatusPath;
+        return false;
+    }
+    return std::stoi(android::base::Trim(buf)) == 1;
+}
+
+Return<bool> AntiFlicker::setEnabled(bool enabled) {
+    if (!android::base::WriteStringToFile((enabled ? "1" : "0"), kDcDimmingStatusPath)) {
+        LOG(ERROR) << "Failed to write " << kDcDimmingStatusPath;
+        return false;
+    }
+    return true;
+}
+
+}  // namespace implementation
+}  // namespace V2_0
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor

livedisplay/AntiFlicker.h
@@ -1,0 +1,47 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VENDOR_LINEAGE_LIVEDISPLAY_V2_0_ANTIFLICKER_H
+#define VENDOR_LINEAGE_LIVEDISPLAY_V2_0_ANTIFLICKER_H
+
+#include <hidl/MQDescriptor.h>
+#include <hidl/Status.h>
+#include <vendor/lineage/livedisplay/2.0/IAntiFlicker.h>
+
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace V2_0 {
+namespace implementation {
+
+using ::android::sp;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+
+class AntiFlicker : public IAntiFlicker {
+  public:
+    // Methods from ::vendor::lineage::livedisplay::V2_0::IAntiFlicker follow.
+    Return<bool> isEnabled() override;
+    Return<bool> setEnabled(bool enabled) override;
+};
+
+}  // namespace implementation
+}  // namespace V2_0
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
+
+#endif  // VENDOR_LINEAGE_LIVEDISPLAY_V2_0_ANTIFLICKER_H

DerpFest-Devices/device_xiaomi_sm6150-common

Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

livedisplay/AntiFlicker.cpp
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "AntiFlickerService"
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android-base/strings.h>
+
+#include "AntiFlicker.h"
+
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace V2_1 {
+namespace implementation {
+
+static constexpr const char* kAntiFlickerStatusPath =
+        "/sys/devices/platform/soc/soc:qcom,dsi-display/msm_fb_ea_enable";
+
+Return<bool> AntiFlicker::isEnabled() {
+    std::string buf;
+    if (!android::base::ReadFileToString(kAntiFlickerStatusPath, &buf)) {
+        LOG(ERROR) << "Failed to read " << kAntiFlickerStatusPath;
+        return false;
+    }
+    return std::stoi(android::base::Trim(buf)) == 1;
+}
+
+Return<bool> AntiFlicker::setEnabled(bool enabled) {
+    if (!android::base::WriteStringToFile((enabled ? "1" : "0"), kAntiFlickerStatusPath)) {
+        LOG(ERROR) << "Failed to write " << kAntiFlickerStatusPath;
+        return false;
+    }
+    return true;
+}
+
+}  // namespace implementation
+}  // namespace V2_1
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor

livedisplay/AntiFlicker.h
@@ -1,0 +1,47 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VENDOR_LINEAGE_LIVEDISPLAY_V2_1_ANTIFLICKER_H
+#define VENDOR_LINEAGE_LIVEDISPLAY_V2_1_ANTIFLICKER_H
+
+#include <hidl/MQDescriptor.h>
+#include <hidl/Status.h>
+#include <vendor/lineage/livedisplay/2.1/IAntiFlicker.h>
+
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace V2_1 {
+namespace implementation {
+
+using ::android::sp;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+
+class AntiFlicker : public IAntiFlicker {
+  public:
+    // Methods from ::vendor::lineage::livedisplay::V2_1::IAntiFlicker follow.
+    Return<bool> isEnabled() override;
+    Return<bool> setEnabled(bool enabled) override;
+};
+
+}  // namespace implementation
+}  // namespace V2_1
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
+
+#endif  // VENDOR_LINEAGE_LIVEDISPLAY_V2_1_ANTIFLICKER_H