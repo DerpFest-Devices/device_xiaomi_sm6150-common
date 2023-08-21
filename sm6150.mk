eb05d7063e2a524ebf66f94e1b2403f4b376f047
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-08-21 13:21:18 +0530

    sm6150-common: Introduce Dolby Atmos app
    Converted existing dirac implementation based on observing stock
    sound effects app and daxservice.
    
    Thanks to jhenrique09 for the hack from old dirac parts
    "Pause/play music stream to get effects applied".
    
    TODO: bring back misound (same as stock)
    
    Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
    Co-authored-by: Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
    Change-Id: I2de96ba526284dbbb7eb6c85d1963cf5937e8664
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -161,6 +161,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0 \
     vendor.qti.hardware.display.mapper@4.0.vendor \
 
+# Dolby
+PRODUCT_PACKAGES += \
+    XiaomiDolby
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4.vendor \
