ccf8902484bee3f151b96a99fed46bc0b2ecfb3c
basamaryan <basam.aryan@gmail.com>
2023-08-28 17:27:14 -0400

    sm6150-common: Enable anti_flicker by default
    Change-Id: Ifc93157308f57ca0062f61347d158ebc96db912b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -149,7 +149,7 @@ SOONG_CONFIG_NAMESPACES += xiaomiSm6150Vars
 SOONG_CONFIG_xiaomiSm6150Vars += \
     livedisplay_support_anti_flicker \
     livedisplay_support_sunlight_enhancement
-SOONG_CONFIG_xiaomiSm6150Vars_livedisplay_support_anti_flicker ?= false
+SOONG_CONFIG_xiaomiSm6150Vars_livedisplay_support_anti_flicker ?= true
 SOONG_CONFIG_xiaomiSm6150Vars_livedisplay_support_sunlight_enhancement ?= true
 
 # Verified Boot
