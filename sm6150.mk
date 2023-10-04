a86408872a884ed0e03ca6d8d6399eb169f8adb7
Tim Zimmermann <tim@linux4.de>
2023-10-04 19:47:23 +0530

    sm6150-common: Patch widevine to load libprotobuf-cpp-full-3.9.1
    * Patch widevine to load full variant as there's no lite vendorcompat variant
    
    Change-Id: I8d1fbc40bebf66abea342780b02cda68d64899b1
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -63,6 +63,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl:32 \
