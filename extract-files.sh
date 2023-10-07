51d1732798dceb66a77e1000a7b0fd3da6591ef9
adi8900 <adrianszymanski242@gmail.com>
2023-10-07 23:13:48 +0200

    sm6150-common: Patch more blobs to use full protobuf
    grep -r libprotobuf-cpp-lite-3.9.1.so
    grep: sm6150-common/proprietary/vendor/lib64/libwvhidl.so: binary file matches
    grep: sm6150-common/proprietary/vendor/lib64/mediadrm/libwvdrmengine.so: binary file matches
    grep: davinci/proprietary/vendor/lib64/libssc.so: binary file matches
    grep: davinci/proprietary/vendor/lib64/libsnsapi.so: binary file matches
    grep: davinci/proprietary/vendor/lib64/libsensorcal.so: binary file matches
    grep: davinci/proprietary/vendor/lib64/libsnsdiaglog.so: binary file matches
    grep: davinci/proprietary/vendor/lib64/sensors.ssc.so: binary file matches
    grep: davinci/proprietary/vendor/bin/sensors.qti: binary file matches
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -95,10 +95,7 @@ function blob_fixup() {
         $PATCHELF_TOOL --remove-needed "libmegface.so" "${2}"
         $PATCHELF_TOOL --add-needed "libshim_megvii.so" "${2}"
     ;;
-    vendor/lib64/libwvhidl.so)
-        $PATCHELF_TOOL --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
-    ;;
-    vendor/lib64/mediadrm/libwvdrmengine.so)
+    vendor/lib64/mediadrm/libwvdrmengine.so | vendor/lib64/libssc.so | vendor/lib64/libsnsapi.so | vendor/lib64/libsensorcal.so | vendor/lib64/libsnsdiaglog.so | vendor/lib64/sensors.ssc.so | vendor/bin/sensors.qti | vendor/lib64/libwvhidl.so)
          $PATCHELF_TOOL  --replace-needed "libprotobuf-cpp-lite-3.9.1.so" "libprotobuf-cpp-full-3.9.1.so" "${2}"
     ;;
     vendor/etc/init/vendor.sensors.qti.rc )
