9fc1ecd1e3a40974114e5f607830327549eca41c
Kelvin Zhang <zhangkelvin@google.com>
2024-05-28 09:19:54 -0700

    sm6150-common: Allow to access bootable/deprecated-ota targets
    sm6150-common devices are still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to sm6150-common namespace list ahead of time to avoid
    breakage.
    
    Test: th
    Bug: 324360816
    Change-Id: I909064be36e8febe80492cf010eb6eac3d6bc33d

--- sm6150.mk
+++ sm6150.mk
@@ -358,6 +358,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
