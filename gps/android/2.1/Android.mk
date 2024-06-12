28106919347d87864d0c180631ce6997486623fd
adi8900 <adrianszymanski242@gmail.com>
2024-06-12 22:06:37 +0200

    Revert "sm6150-common: gps: Add dependencies for Health AIDL change"
    This reverts commit 400e0023b5f588651ea269ca9fe3196d8f15313f.

--- gps/android/2.1/Android.mk
+++ gps/android/2.1/Android.mk
@@ -41,7 +41,6 @@ LOCAL_HEADER_LIBRARIES := \
     liblocbatterylistener_headers
 
 LOCAL_SHARED_LIBRARIES := \
-    libbinder_ndk \
     liblog \
     libhidlbase \
     libcutils \
@@ -56,7 +55,6 @@ LOCAL_SHARED_LIBRARIES := \
     android.hardware.health@1.0 \
     android.hardware.health@2.0 \
     android.hardware.health@2.1 \
-    android.hardware.health-V1-ndk \
     android.hardware.power@1.2 \
     libbase
 
