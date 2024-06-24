775b5f300d0fdfba29d94f9a50d2b59efc2b52b1
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-24 16:47:29 +0800

    sm6150-common: Migrate to QTI USB Gadget 1.2 HIDL
    Change-Id: I6adbca2fab9a3fe0477d17f9786166828b786710
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -387,7 +387,9 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb
+    usb_compositions.conf \
+    android.hardware.usb@1.3-service.dual_role_usb \
+    android.hardware.usb.gadget@1.2-service-qti
 
 # Vibrator
 PRODUCT_PACKAGES += \
