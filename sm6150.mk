fc358786d03c261b172f7dd360b030da2ae8e1cc
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-06-25 03:30:21 +0530

    sm6150-common: Move to common Xiaomi light AIDL
    [@Joker-V2: Keep permissions and move it into init.qcom.rc
    
    Signed-off-by: Joker-V2 <nipinna0@gmail.com>
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -202,7 +202,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.lights-service.xiaomi_sm6150
+    android.hardware.light-service.xiaomi
 
 # Lineage Health
 PRODUCT_PACKAGES += \
