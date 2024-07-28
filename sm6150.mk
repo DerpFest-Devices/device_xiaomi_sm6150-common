b5ceb42a022baabc273b7407c0af35beb6278e47
adi8900 <adrianszymanski242@gmail.com>
2024-07-28 21:39:36 +0200

    sm6150-common: Disable blur
    * It is too much for our sm6150 based devices, they are sluggish as hell with enabled blurs.
    
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -110,6 +110,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     audio.bluetooth.default
 
+# Blur
+TARGET_NOT_USES_BLUR := true
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:64 \
