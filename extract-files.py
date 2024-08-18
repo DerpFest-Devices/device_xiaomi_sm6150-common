1333ab2a6c202d96eebd32dceb9144b92f381e9a
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-18 09:52:31 +0300

    sm6150-common: skip dependencies which aren't defined in .bp
    Change-Id: I01a653661856495788c05b720c0577dac44998a4
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -42,6 +42,10 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.qti.hardware.fm@1.0',
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
+    (
+        'libOmxCore',
+        'libwpa_client',
+    ): lib_fixup_remove,
 }
 
 blob_fixups: blob_fixups_user_type = {
