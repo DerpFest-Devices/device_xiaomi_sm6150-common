032e8c95feb2b67724e1a5812e045ea25b19e1b4
Yumi Yukimura <me.cafebabe@gmail.com>
2024-11-04 20:55:55 +0800

    sm6150-common: Switch to AIDL USB Gadget HAL
    Change-Id: Ic561f62588232449c3bc1e887fc68defea634ecd

--- sm6150.mk
+++ sm6150.mk
@@ -392,7 +392,7 @@ AB_OTA_UPDATER := false
 PRODUCT_PACKAGES += \
     usb_compositions.conf \
     android.hardware.usb@1.3-service.dual_role_usb \
-    android.hardware.usb.gadget@1.2-service-qti
+    android.hardware.usb.gadget-service.qti
 
 # Vibrator
 PRODUCT_PACKAGES += \
