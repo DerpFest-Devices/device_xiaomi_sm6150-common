487edd79d6c093f91b21d8482c49bdacbcef59f2
Nick Reuter <nreuter85@gmail.com>
2024-12-07 15:59:17 -0600

    sm6150-common: Bump libwfdservice to android.media.audio.common.types-V4
    Change-Id: I81060d8c73430a9598c676e18dc04e31a2f31c0c
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -86,6 +86,9 @@ function blob_fixup() {
     system_ext/lib64/libwfdnative.so | vendor/lib64/libgoodixhwfingerprint.so )
         $PATCHELF_TOOL --remove-needed "android.hidl.base@1.0.so" "${2}"
     ;;
+    system_ext/lib/libwfdservice.so )
+	$PATCHELF_TOOL --replace-needed "android.media.audio.common.types-V3-cpp.so" "android.media.audio.common.types-V4-cpp.so" "${2}"
+   ;;
     vendor/etc/camera/camxoverridesettings.txt )
         sed -i "s|0x10080|0|g" "${2}"
         sed -i "s|0x1F|0x0|g" "${2}"
