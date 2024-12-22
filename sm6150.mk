4ecf466f37bdcab327693fcb4547d8c8d894d659
adi8900 <adrianszymanski242@gmail.com>
2024-12-22 17:27:58 +0100

    sm6150-common: Update system WFD blobs
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -429,7 +429,7 @@ PRODUCT_PACKAGES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    android.media.audio.common.types-V2-cpp \
+    android.media.audio.common.types-V3-cpp \
     libavservices_minijail_32 \
     libdisplayconfig.system.qti \
     libpng.vendor:32 \
