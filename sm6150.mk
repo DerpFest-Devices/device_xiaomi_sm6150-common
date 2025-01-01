75b90e344a3e2fc151f2a92c94e335c1d9b8409e
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 03:50:53 +0200

    sm6150-common: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Ifc4e10703d3424607b9dc8bcc8e417d1c88cc2f3
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -145,8 +145,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    gralloc.sm6150 \
-    hwcomposer.sm6150 \
+    hwcomposer.qcom \
     libdisplayconfig.qti \
     libtinyxml \
     libvulkan \
