1f2c97bc2616b3bb0337caeb2e645d4d80e6ad96
shoya0x00 <shoya0x00@gmail.com>
2025-04-07 13:35:18 +0000

    sm6150-common: Import C2 Sony Dolby Atmos
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -118,6 +118,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
 
+# Dolby
+$(call inherit-product, vendor/sony/dolby/dolby.mk)
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
