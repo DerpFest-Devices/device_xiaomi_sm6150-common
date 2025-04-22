cc02a8b874154d5351e87b5c66deb439417fded6
adi8900 <adrianszymanski242@gmail.com>
2025-04-22 09:16:32 +0200

    sm6150-common: Derp ---> Lineage
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -85,7 +85,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/derp/config/device_framework_matrix.xml
+    vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
@@ -146,7 +146,7 @@ VENDOR_SECURITY_PATCH := 2024-05-01
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/derp/sepolicy/libperfmgr/sepolicy.mk
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
