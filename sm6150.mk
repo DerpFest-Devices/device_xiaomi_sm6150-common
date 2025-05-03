094539aad27a056d5a1d47a9f81d9e0deaf95f48
adi8900 <adrianszymanski242@gmail.com>
2025-05-03 19:32:25 +0200

    sm6150-common: fixup! Set libperfmgr mode extension lib via soong config
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -236,7 +236,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
-$(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(COMMON_PATH):libperfmgr-ext-xiaomi)
+$(call soong_config_set,power_libperfmgr,mode_extension_lib,//$(LOCAL_PATH):libperfmgr-ext-xiaomi)
 
 # QTI
 PRODUCT_COPY_FILES += \
