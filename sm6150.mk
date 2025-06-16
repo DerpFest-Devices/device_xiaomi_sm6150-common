fe1d11982e10e3f288958b806409990796df079a
basamaryan <basam.aryan@gmail.com>
2025-06-16 11:19:48 -0400

    sm6150-common: Drop legacy ANT remnants
    QSSI only has platform/external/ant-wireless/hidl since a while
    
    Change-Id: Ibccfdc7e72efd3c9d3ff62bda3e729b99433a567
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -104,10 +104,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
-# ANT+
-PRODUCT_PACKAGES += \
-    AntHalService-Soong
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
