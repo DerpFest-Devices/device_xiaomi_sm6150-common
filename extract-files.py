raul-16/android_device_xiaomi_sm6150-common

sm6150-common: Add libwfdservice_shim for WFD
e69a5be2449122f90220151f9d01ace15221facf in system/hardware/interfaces
broke the ABI ;'(

Change-Id: I3badfc56581a6bf3de5d7e996fa51f59eb164aae | AOSP

extract-files.py
@@ -46,6 +46,8 @@ lib_fixups: lib_fixups_user_type = {
 
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/bin/wfdservice64': blob_fixup()
+        .add_needed('libwfdservice_shim.so'),
     'system_ext/etc/init/wfdservice.rc': blob_fixup()
         .regex_replace(r'(start|stop) wfdservice\b', r'\1 wfdservice64'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()

DerpFest-Devices/device_xiaomi_sm6150-common

Change-Id: I3badfc56581a6bf3de5d7e996fa51f59eb164aae
Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

extract-files.py
@@ -49,6 +49,8 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/bin/wfdservice': blob_fixup()
+         .add_needed('libwfdservice_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .remove_needed('android.hidl.base@1.0.so'),
     'system_ext/lib/libwfdservice.so': blob_fixup()