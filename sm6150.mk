4e4ca085d8ebe192e5d42fae32c1ac93e8bd07be
adi8900 <adrianszymanski242@gmail.com>
2025-09-02 10:03:40 +0200

    sm6150-common: Add miuicamera support
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -392,3 +392,5 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.zram.mark_idle_delay_mins=60 \
     ro.zram.first_wb_delay_mins=1440 \
     ro.zram.periodic_wb_delay_hours=24
+
+$(call inherit-product, vendor/xiaomi/miuicamera/config.mk)
