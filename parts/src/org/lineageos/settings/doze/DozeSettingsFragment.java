d258772cdda15009024161ebfce6d6486562dadd
Bruno Martins <bgcngm@gmail.com>
2025-12-07 09:40:50 +0000

    sm6150-common: parts: Clean up imports
    Change-Id: I483a137865e0e558ecadfb1ed25d0f46daf0f3b2

--- parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
+++ parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -19,10 +19,7 @@ package org.lineageos.settings.doze;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
