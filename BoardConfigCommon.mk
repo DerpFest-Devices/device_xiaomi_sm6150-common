Justadeayo/device_xiaomi_violet

sm6150-common: Use legacy libion implementation
* At least one blob directly depends on this, with
  many other bits and pieces likely.

Change-Id: Ic063618d6942a4f6e90dd7e21698e36d14113d06
Signed-off-by: Frost <frost.github@proton.me>

BoardConfigCommon.mk
@@ -136,6 +136,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
+include device/voltage/sepolicy/libion/sepolicy.mk
 include device/voltage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 

DerpFest-Devices/device_xiaomi_sm6150-common

Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

BoardConfigCommon.mk
@@ -133,6 +133,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
+include device/lineage/sepolicy/libion/sepolicy.mk
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 