cf992e64994835f533ab8e4fa84ccd16337320e2
adi8900 <adrianszymanski242@gmail.com>
2026-09-14 15:40:45 +0200

    sm6150-common: Update system wfd blobs from qssi_64
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -46,7 +46,7 @@ lib_fixups: lib_fixups_user_type = {
 
 
 blob_fixups: blob_fixups_user_type = {
-    ('system_ext/lib/libwfdservice.so', 'system_ext/lib64/libwfdservice.so'): blob_fixup()
+    'system_ext/lib64/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V4-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'vendor/lib64/libdpps.so': blob_fixup()
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
